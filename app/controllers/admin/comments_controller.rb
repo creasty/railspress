f2e5fea4de3a3a527cf256b5fe55fb90571c911a
Yuki Iwanaga <yuki@creasty.com>
2013-06-23 00:23:54 +0900

    comments のデザイン途中 #18

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,7 +2,11 @@
 class Admin::CommentsController < Admin::ApplicationController
 
   def index
-    @comments = Comment.order 'created_at DESC'
+    @comments = Comment
+      .group('post_id')
+      .order('created_at DESC')
+      .includes :post
+
   end
 
 end
