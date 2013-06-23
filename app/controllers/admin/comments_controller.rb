6b5f15696d55988a4d7e9a50834015a8cdef4454
Yuki Iwanaga <yuki@creasty.com>
2013-06-23 15:10:29 +0900

    デザインはこれでいいかな #18

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,11 +2,26 @@
 class Admin::CommentsController < Admin::ApplicationController
 
   def index
-    @comments = Comment
-      .group('post_id')
-      .order('created_at DESC')
-      .includes :post
+    respond_to do |format|
+      format.json do
+        @comments = Comment
+          .group('post_id')
+          .order('created_at DESC')
+          .includes :post
 
+        render json: @comments.map { |comment| comment.to_backbone_json }
+      end
+      format.html { render }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      @comment = Comment
+        .where('post_id = ?', params[comment][:post_id])
+        .order('created_at DESC')
+        .includes :post
+    end
   end
 
 end
